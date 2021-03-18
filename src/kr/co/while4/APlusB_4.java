981cdb55ac727868fe0da93b63b7f45e21cbc3eb
Mina Jung <octavesop@gmail.com>
2021-03-18 09:40:46 +0900

    while 단원 실수
    A+B_4랑 A+B_5 바꾸기

--- src/kr/co/while4/APlusB_4.java
+++ src/kr/co/while4/APlusB_4.java
@@ -1,14 +1,17 @@
 package kr.co.while4;
-
 import java.util.Scanner;
 public class APlusB_4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        while (sc.hasNextInt())
-        {
-            int A = sc.nextInt();
-            int B = sc.nextInt();
-            System.out.println(A+B);
+        while(true) {
+            int num = sc.nextInt();
+            int num2 = sc.nextInt();
+            if(num+num2==0) {
+                break;
+            }
+            else {
+                System.out.println(num+num2);
+            }
         }
     }
 }
--- src/kr/co/while4/APlusB_5.java
+++ src/kr/co/while4/APlusB_5.java
@@ -3,15 +3,11 @@ import java.util.Scanner;
 public class APlusB_5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        while(true) {
-            int num = sc.nextInt();
-            int num2 = sc.nextInt();
-            if(num+num2==0) {
-                break;
-            }
-            else {
-                System.out.println(num+num2);
-            }
+        while (sc.hasNextInt())
+        {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            System.out.println(A+B);
         }
     }
 }
