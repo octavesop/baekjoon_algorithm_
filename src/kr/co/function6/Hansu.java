b79368101a050ab6c8070a313badd50a3c5e765d
Mina Jung <octavesop@gmail.com>
2021-03-21 23:53:18 +0900

    한수 문제를 풀었다!
    야호

--- src/kr/co/function6/Hansu.java
+++ src/kr/co/function6/Hansu.java
@@ -1,4 +1,26 @@
 package kr.co.function6;
-
+import java.util.Scanner;
 public class Hansu {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int han=99;
+        int hundredToTen;
+        int tenToOne;
+
+        for(int i=100; i<=n; i++) {
+            hundredToTen = i / 100 - i / 10 % 10;
+            tenToOne = i / 10 % 10 - i % 10;
+            if (hundredToTen == tenToOne) {
+                han++;
+            }
+        }
+
+        if (n<100) {
+            System.out.println(n);
+        } else {
+            System.out.println(han);
+        }
+    }
 }
+
