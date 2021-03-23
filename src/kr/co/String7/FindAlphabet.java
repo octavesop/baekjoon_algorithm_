1f4c4e98fd88099744e64bb2b4089588e001b461
Mina Jung <octavesop@gmail.com>
2021-03-23 17:29:05 +0900

    코드 수정

--- src/kr/co/String7/FindAlphabet.java
+++ src/kr/co/String7/FindAlphabet.java
@@ -2,31 +2,12 @@ package kr.co.String7;
 import java.util.Scanner;
 public class FindAlphabet {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        int[] arr = new int[26];
-        String str = sc.next();
+        String temp = sc.next();
 
-        for (int i = 0; i < str.length(); i++) {
-
-            if ('A' <= str.charAt(i) && str.charAt(i) <= 'Z') {
-                arr[str.charAt(i) - 'A']++;
-            } else {
-                arr[str.charAt(i) - 'a']++;
-            }
+        for (char i = 'a'; i <= 'z'; i++) {
+            System.out.print(temp.indexOf(i) + " ");
         }
-
-
-        int max = -1;
-        char c = '?';
-
-        for (int i = 0; i < 26; i++) {
-            if (arr[i] > max) {
-                max = arr[i];
-                c = (char) (i + 65);
-            } else if (arr[i] == max) {
-                c = '?';
-            }
-        }
-        System.out.print(c);
     }
 }
